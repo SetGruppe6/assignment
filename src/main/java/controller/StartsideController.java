e52a709a8ece5075a44406350b0e5054808ea972
blendimaliqi <blend-i>
2019-10-24 14:45:49 +0200

    Bruker knappen bytter til brukerscene

--- src/main/java/controller/StartsideController.java
+++ src/main/java/controller/StartsideController.java
@@ -1,52 +1,21 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import model.Bruker;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class StartsideController {
 
-    @FXML
-    private Button registrerButton;
-
-    @FXML
-    private TextField emailTextField;
-
-    @FXML
-    private PasswordField passordPasswordField;
-
-    @FXML
-    private TextField navnTextField;
-
-    @FXML
-    private TextField etternavnTextField;
-
-    private ArrayList<Bruker> nyBrukerListe;
-
-
-    @FXML
-    public void initialize(){
-
-        registrerButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-
-                String email = emailTextField.getText();
-                String pass = passordPasswordField.getText();
-
-                Bruker nyBruker = new Bruker(email, pass);
-
-                nyBrukerListe.add(nyBruker);
-            }
-        });
-
-
+    public void byttTilBrukerScene(ActionEvent event) throws IOException {
+        Parent brukerParent = FXMLLoader.load(getClass().getResource("/brukerside.fxml"));
+        Scene brukerScene = new Scene(brukerParent);
+        Stage vindu = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        vindu.setScene(brukerScene);
+        vindu.show();
     }
-
 }
