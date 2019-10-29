b2e28a7aa52a3a0d0d5dae455f81d0db77f1023d
blendimaliqi <blend-i>
2019-10-29 11:54:16 +0100

    Laget admin grensesnitt

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -20,4 +19,6 @@ public class AdminController {
         vindu.setScene(brukerScene);
         vindu.show();
     }
+
+
 }
