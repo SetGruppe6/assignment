40c9ff554793c8145a4238ce74a9066e97a8c3d3
Are <aregulliksen@gmail.com>
2019-10-31 16:39:05 +0100

    Opprettet Alertbox som åpnes når arrangementet skal opprettes.

--- src/main/java/controller/OpprettArrangementController.java
+++ src/main/java/controller/OpprettArrangementController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -22,6 +23,20 @@ public class OpprettArrangementController {
     @FXML
     private TextField tittelTextField;
 
+    @FXML
+    private TextField stedTextField;
+
+    @FXML
+    private TextField startTextField;
+
+    @FXML
+    private TextField sluttTextField;
+
+    @FXML
+    private TextField kapasitetTextField;
+
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+
     public void tilbakeFraOpprettArrangement(ActionEvent event) throws IOException {
         Parent brukerParent = FXMLLoader.load(getClass().getResource("/adminside.fxml"));
         Scene brukerScene = new Scene(brukerParent);
@@ -36,6 +51,18 @@ public class OpprettArrangementController {
 
             /*Ha dette i en if-test som sjekker om returen på erTittelOk*/
             Arrangement.erTittelOk(tittelTextField.getText());
+            Arrangement.erLokasjonGitt(stedTextField.getText());
+            Arrangement.erTidsromGitt(startTextField.getText(), sluttTextField.getText());
+            Arrangement.erDeltakerKapasitetOk(Integer.parseInt(kapasitetTextField.getText()));
+
+            alert.setTitle("Advarsel");
+            alert.setHeaderText("Det er noen feil i skjemaet.");
+            alert.setContentText(Arrangement.erTittelOk(tittelTextField.getText()) + " \n" +
+            Arrangement.erLokasjonGitt(stedTextField.getText()) + " \n" +
+            Arrangement.erTidsromGitt(startTextField.getText(), sluttTextField.getText()) + " \n" +
+            Arrangement.erDeltakerKapasitetOk(Integer.parseInt(kapasitetTextField.getText())));
+
+            alert.showAndWait();
 
         }
     };
