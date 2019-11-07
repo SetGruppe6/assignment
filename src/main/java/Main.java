2eaf73ed45506ffbd5ed2a0b8d20542c4e740679
mienna91 <43706477+mienna91@users.noreply.github.com>
2019-11-07 15:36:41 +0100

    Setter prisen til 0 på ene dummy arrangmentet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        //Legger til noen dummy arrangementer bare for at sidene skal vise noe fra starten av
         Arrangement.leggTilDummyArrangementer();
     }
 
