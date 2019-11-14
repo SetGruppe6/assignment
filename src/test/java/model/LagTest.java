9f51a4372b4d696b39ce0e34ee8454c7987b550f
Are <aregulliksen@gmail.com>
2019-11-14 11:58:51 +0100

    Laget nye tester i Arrangementtest og LagTest

--- src/test/java/model/LagTest.java
+++ src/test/java/model/LagTest.java
@@ -2,17 +2,50 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LagTest {
 
-    Lag TestLag = new Lag("Testlaget", new ArrayList<Person>());
+
+    Ski TestArrangementSki = new Ski("Birk", "", LocalDate.of(2018, 2, 3), LocalTime.of(20,30), LocalTime.of(10,30), 0, -200, "", new ArrayList<>());
+    Lop TestArrangementLop = new Lop("Oslo Maraton", "Oslo, Rådhusplassen", LocalDate.of(2020, 9, 19), LocalTime.of(13,0), LocalTime.of(19,0), 900, 299, "Oslo Maraton er Norges største maraton og arrangeres hver høst i Oslos gater av Sportsklubben Vidar", new ArrayList<>());
+    Lag TestLag = new Lag("Testlaget", new ArrayList<Person>(), new ArrayList<Arrangement>());
     Person TestJon = new Person("Jon","Jonsen");
     Person TestPer = new Person("Per", "Persson");
     Person TestErik = new Person("Erik","Ericcson");
 
+
+    @Test
+    public void meldPaaArrangementTest(){
+        ArrayList<Arrangement> paameldteArrangement = new ArrayList<>();
+        paameldteArrangement.add(TestArrangementLop);
+        TestLag.meldPaaArrangement(TestArrangementLop);
+        assertEquals(paameldteArrangement,TestLag.getArrangementerLagetErPaameldt());
+    }
+
+    @Test
+    public void avmeldArrangementTest(){
+        ArrayList<Arrangement> paameldteArrangement = new ArrayList<>();
+        paameldteArrangement.add(TestArrangementSki);
+        TestLag.meldPaaArrangement(TestArrangementLop);
+        TestLag.meldPaaArrangement(TestArrangementSki);
+        TestLag.avmeldArrangement(TestArrangementLop);
+        assertEquals(paameldteArrangement,TestLag.getArrangementerLagetErPaameldt());
+    }
+
+    @Test
+    public void paameldteArrangementerTest(){
+        ArrayList<Arrangement> paameldteArrangementTest = new ArrayList<>();
+        paameldteArrangementTest.add(TestArrangementSki);
+        paameldteArrangementTest.add(TestArrangementLop);
+        TestArrangementLop.leggTilDeltaker(TestJon);
+        TestArrangementSki.leggTilDeltaker(TestJon);
+        assertEquals(paameldteArrangementTest, TestLag.paameldteArrangementer(paameldteArrangementTest));
+    }
 
     @Test
     public void erEttLagmedlemLagtTilTest(){
