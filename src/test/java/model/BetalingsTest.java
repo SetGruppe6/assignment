ffb9a4a0554cd8df52f501624744484a179a60ea
Are <aregulliksen@gmail.com>
2019-11-14 13:44:09 +0100

    La inn Assertions

--- src/test/java/model/BetalingsTest.java
+++ src/test/java/model/BetalingsTest.java
@@ -1,13 +1,22 @@
 package model;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BetalingsTest {
 
     @Test
     public void harBetaltTest(){
         boolean betalt = false;
+        Betaling betaling = new Betaling(betalt);
+        betaling.harBetalt();
+        assertTrue(betaling.isBetalt());
+    }
 
-
+    @Test
+    public void harIkkeBetaltTest(){
+        boolean betalt = false;
+        Betaling betaling = new Betaling(betalt);
+        assertFalse(betaling.isBetalt());
     }
 }
